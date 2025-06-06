a15e216a29fa9c708c517a03b779fb5b97153908
petrykkarina <karina.petryk.ir.2024@lpnu.ua>
2025-06-06 21:34:17 +0300

    test_tree_depth.py

--- test/test_tree_depth.py
+++ test/test_tree_depth.py
@@ -8,7 +8,6 @@ from tree_depth import TreeNode, sum_of_depths
 
 class TestSumOfDepths(unittest.TestCase):
     def test_example(self):
-        # Приклад з умови
         root = TreeNode(1)
         root.left = TreeNode(2)
         root.right = TreeNode(3)
@@ -24,19 +23,15 @@ class TestSumOfDepths(unittest.TestCase):
         self.assertEqual(sum_of_depths(root), 0)
 
     def test_left_skewed_tree(self):
-        # дерево: 1 -> 2 -> 3
         root = TreeNode(1)
         root.left = TreeNode(2)
         root.left.left = TreeNode(3)
-        # глибини: 0 + 1 + 2 = 3
         self.assertEqual(sum_of_depths(root), 3)
 
     def test_right_skewed_tree(self):
-        # дерево: 1 -> 2 -> 3
         root = TreeNode(1)
         root.right = TreeNode(2)
         root.right.right = TreeNode(3)
-        # глибини: 0 + 1 + 2 = 3
         self.assertEqual(sum_of_depths(root), 3)
 
 
