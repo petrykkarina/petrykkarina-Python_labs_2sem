ec159afe4f4a1c1ea6b585d57704d38f9990f61b
petrykkarina <karina.petryk.ir.2024@lpnu.ua>
2025-06-06 22:27:42 +0300

    unitest1.py

--- test/unitest1.py
+++ test/unitest1.py
@@ -1,9 +1,10 @@
 import unittest
 import sys
 import os
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
-from lab6 import max_experience
+from lab6 import max_experience_with_path
 
 class TestCareer(unittest.TestCase):
     def test_example1(self):
@@ -13,13 +14,15 @@ class TestCareer(unittest.TestCase):
             [2, 1, 5],
             [1, 3, 2, 1]
         ]
-        self.assertEqual(max_experience(levels), 12)
+        experience, _ = max_experience_with_path(levels)
+        self.assertEqual(experience, 12)
 
     def test_example2(self):
         levels = [
             [9999]
         ]
-        self.assertEqual(max_experience(levels), 9999)
+        experience, _ = max_experience_with_path(levels)
+        self.assertEqual(experience, 9999)
 
     def test_example3(self):
         levels = [
@@ -29,18 +32,21 @@ class TestCareer(unittest.TestCase):
             [1, 1, 1, 1],
             [0, 1, 0, 1, 0]
         ]
-        self.assertEqual(max_experience(levels), 3)
+        experience, _ = max_experience_with_path(levels)
+        self.assertEqual(experience, 3)
 
     def test_single_zero(self):
         levels = [
             [0]
         ]
-        self.assertEqual(max_experience(levels), 0)
+        experience, _ = max_experience_with_path(levels)
+        self.assertEqual(experience, 0)
 
     def test_large_simple(self):
         levels = [list(range(1, i + 2)) for i in range(1000)]
         expected = sum(i + 1 for i in range(1000))
-        self.assertEqual(max_experience(levels), expected)
+        experience, _ = max_experience_with_path(levels)
+        self.assertEqual(experience, expected)
 
 if __name__ == '__main__':
     unittest.main()
